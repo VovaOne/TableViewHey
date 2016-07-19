b9dad9fe2e15d32f545998212add970146a94944
Vladimir S. Kiryakov <vladimir.kiryakov@jazzsoft.kz>
2016-07-19 14:46:48 +0600

    custom cell

---
+++ TableViewHey/Photo.swift
@@ -0,0 +1,9 @@
+//
+//  Photo.swift
+//  TableViewHey
+//
+//  Created by Vladimir S. Kiryakov on 19.07.16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+import Foundation
---
+++ TableViewHey/PhotoTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoTableViewCell.swift
+//  TableViewHey
+//
+//  Created by Vladimir S. Kiryakov on 19.07.16.
+//  Copyright © 2016 test. All rights reserved.
+//
+
+import Cocoa
+
+class PhotoTableViewCell: UITableViewCell {
+
+}
